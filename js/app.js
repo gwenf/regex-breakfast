91cb9ae84c96d370612a7fbfa9d70c5389915e41
gwenf <gwenfaraday@gmail.com>
2017-01-12 12:37:03 -0500

    basic imperative code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('working')
+var arrayOfImages = [
+    'coffee-cup.png',
+    'cereal.png',
+    'plate.png',
+    'toaster.png'
+]
+
+$('#logo-image').on('click', function(){
+    var imageNum = parseInt($(this).attr('data-number'))
+    imageNum = imageNum < 3 ? imageNum + 1 : 0;
+    $(this).attr('data-number', imageNum)
+    $(this).attr('src', 'images/' + arrayOfImages[imageNum])
+})
+
+$('.expanding-button').on('click', function () {
+    var element = $(this).attr('data-reveal')
+    $('.'+element).slideToggle(300);
+    console.log(element, $('.'+element))
+});
